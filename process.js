085f21eb9c83ce164e7cfdbd0aa7339a230253fe
LgoAstrounaut <adityaraghav_ug@ece.nits.ac.in>
2021-07-09 19:08:36 +0530

    create Typing Test

---
+++ process.js
@@ -0,0 +1,286 @@
+var word = [ "the" , "of" , "to" , "and" , "a" , "in" , "is" , "it" , "you" , "that" , "he" , "was" , "for" , "on" , "are" , "with" , "as" , "I" , "his" , "they" , "be" , "at" , "one" , "have" , "this" , "from" , "or" , "had" , "by" , "not" , "word" , "but" , "what" , "some" , "we" , "can" , "out" , "other" , "were" , "all" , "there" , "when" , "up" , "use" , "your" , "how" , "said" , "an" , "each" , "she" , "which" , "do" , "their" , "time" , "if" , "will" , "way" , "about" , "many" , "then" , "them" , "write" , "would" , "like" , "so" , "these" , "her" , "long" , "make" , "thing" , "see" , "him" , "two" , "has" , "look" , "more" , "day" , "could" , "go" , "come" , "did" , "number" , "sound" , "no" , "most" , "people" , "my" , "over" , "know" , "water" , "than" , "call" , "first" , "who" , "may" , "down" , "side" , "been" , "now" , "find" , "any" , "new" , "work" , "part" , "take" , "get" , "place" , "made" , "live" , "where" , "after" , "back" , "little" , "only" , "round" , "man" , "year" , "came" , "show" , "every" , "good" , "me" , "give" , "our" , "under" , "name" , "very" , "through" , "just" , "form" , "sentence" , "great" , "think" , "say" , "help" , "low" , "line" , "differ" , "turn" , "cause" , "much" , "mean" , "before" , "move" , "right" , "boy" , "old" , "too" , "same" , "tell" , "does" , "set" , "three" , "want" , "air" , "well" , "also" , "play" , "small" , "end" , "put" , "home" , "read" , "hand" , "port" , "large" , "spell" , "add" , "even" , "land" , "here" , "must" , "big" , "high" , "such" , "follow" , "act" , "why" , "ask" , "men" , "change" , "went" , "light" , "kind" , "off" , "need" , "house" , "picture" , "try" , "us" , "again" , "animal" , "point" , "mother" , "world" , "near" , "build" , "self" , "earth" , "father" , "head" , "stand" , "own" , "page" , "should" , "country" , "found" , "answer" , "school" , "grow" , "study" , "still" , "learn" , "plant" , "cover" , "food" , "sun" , "four" , "between" , "state" , "keep" , "eye" , "never" , "last" , "let" , "thought" , "city" , "tree" , "cross" , "farm" , "hard" , "start" , "might" , "story" , "saw" , "far" , "sea" , "draw" , "left" , "late" , "run" , "don't" , "while" , "press" , "close" , "night" , "real" , "life" , "few" , "north" , "open" , "seem" , "together" , "next" , "white" , "children" , "begin" , "got" , "walk" , "example" , "ease" , "paper" , "group" , "always" , "music" , "those" , "both" , "mark" , "often" , "letter" , "until" , "mile" , "river" , "car" , "feet" , "care" , "second" , "book" , "carry" , "took" , "science" , "eat" , "room" , "friend" , "began" , "idea" , "fish" , "mountain" , "stop" , "once" , "base" , "hear" , "horse" , "cut" , "sure" , "watch" , "color" , "face" , "wood" , "main" , "enough" , "plain" , "girl" , "usual" , "young" , "ready" , "above" , "ever" , "red" , "list" , "though" , "feel" , "talk" , "bird" , "soon" , "body" , "dog" , "family" , "direct" , "pose" , "leave" , "song" , "measure" , "door" , "product" , "black" , "short" , "numeral" , "class" , "wind" , "question" , "happen" , "complete" , "ship" , "area" , "half" , "rock" , "order" , "fire" , "south" , "problem" , "piece" , "told" , "knew" , "pass" , "since" , "top" , "whole" , "king" , "space" , "heard" , "best" , "hour" , "better" , "true" , "during" , "hundred" , "five" , "remember" , "step" , "early" , "hold" , "west" , "ground" , "interest" , "reach" , "fast" , "verb" , "sing" , "listen" , "six" , "table" , "travel" , "less" , "morning" , "ten" , "simple" , "several" , "vowel" , "toward" , "war" , "lay" , "against" , "pattern" , "slow" , "center" , "love" , "person" , "money" , "serve" , "appear" , "road" , "map" , "rain" , "rule" , "govern" , "pull" , "cold" , "notice" , "voice" , "unit" , "power" , "town" , "fine" , "certain" , "fly" , "fall" , "lead" , "cry" , "dark" , "machine" , "note" , "wait" , "plan" , "figure" , "star" , "box" , "noun" , "field" , "rest" , "correct" , "able" , "pound" , "done" , "beauty" , "drive" , "stood" , "contain" , "front" , "teach" , "week" , "final" , "gave" , "green" , "oh" , "quick" , "develop" , "ocean" , "warm" , "free" , "minute" , "strong" , "special" , "mind" , "behind" , "clear" , "tail" , "produce" , "fact" , "street" , "inch" , "multiply" , "nothing" , "course" , "stay" , "wheel" , "full" , "force" , "blue" , "object" , "decide" , "surface" , "deep" , "moon" , "island" , "foot" , "system" , "busy" , "test" , "record" , "boat" , "common" , "gold" , "possible" , "plane" , "stead" , "dry" , "wonder" , "laugh" , "thousand" , "ago" , "ran" , "check" , "game" , "shape" , "equate" , "hot" , "miss" , "brought" , "heat" , "snow" , "tire" , "bring" , "yes" , "distant" , "fill" , "east" , "paint" , "language" , "among" , "grand" , "ball" , "yet" , "wave" , "drop" , "heart" , "am" , "present" , "heavy" , "dance" , "engine" , "position" , "arm" , "wide" , "sail" , "material" , "size" , "vary" , "settle" , "speak" , "weight" , "general" , "ice" , "matter" , "circle" , "pair" , "include" , "divide" , "syllable" , "felt" , "perhaps" , "pick" , "sudden" , "count" , "square" , "reason" , "length" , "represent" , "art" , "subject" , "region" , "energy" , "hunt" , "probable" , "bed" , "brother" , "egg" , "ride" , "cell" , "believe" , "fraction" , "forest" , "sit" , "race" , "window" , "store" , "summer" , "train" , "sleep" , "prove" , "lone" , "leg" , "exercise" , "wall" , "catch" , "mount" , "wish" , "sky" , "board" , "joy" , "winter" , "sat" , "written" , "wild" , "instrument" , "kept" , "glass" , "grass" , "cow" , "job" , "edge" , "sign" , "visit" , "past" , "soft" , "fun" , "bright" , "gas" , "weather" , "month" , "million" , "bear" , "finish" , "happy" , "hope" , "flower" , "clothe" , "strange" , "gone" , "jump" , "baby" , "eight" , "village" , "meet" , "root" , "buy" , "raise" , "solve" , "metal" , "whether" , "push" , "seven" , "paragraph" , "third" , "shall" , "held" , "hair" , "describe" , "cook" , "floor" , "either" , "result" , "burn" , "hill" , "safe" , "cat" , "century" , "consider" , "type" , "law" , "bit" , "coast" , "copy" , "phrase" , "silent" , "tall" , "sand" , "soil" , "roll" , "temperature" , "finger" , "industry" , "value" , "fight" , "lie" , "beat" , "excite" , "natural" , "view" , "sense" , "ear" , "else" , "quite" , "broke" , "case" , "middle" , "kill" , "son" , "lake" , "moment" , "scale" , "loud" , "spring" , "observe" , "child" , "straight" , "consonant" , "nation" , "dictionary" , "milk" , "speed" , "method" , "organ" , "pay" , "age" , "section" , "dress" , "cloud" , "surprise" , "quiet" , "stone" , "tiny" , "climb" , "cool" , "design" , "poor" , "lot" , "experiment" , "bottom" , "key" , "iron" , "single" , "stick" , "flat" , "twenty" , "skin" , "smile" , "crease" , "hole" , "trade" , "melody" , "trip" , "office" , "receive" , "row" , "mouth" , "exact" , "symbol" , "die" , "least" , "trouble" , "shout" , "except" , "wrote" , "seed" , "tone" , "join" , "suggest" , "clean" , "break" , "lady" , "yard" , "rise" , "bad" , "blow" , "oil" , "blood" , "touch" , "grew" , "cent" , "mix" , "team" , "wire" , "cost" , "lost" , "brown" , "wear" , "garden" , "equal" , "sent" , "choose" , "fell" , "fit" , "flow" , "fair" , "bank" , "collect" , "save" , "control" , "decimal" , "gentle" , "woman" , "captain" , "practice" , "separate" , "difficult" , "doctor" , "please" , "protect" , "noon" , "whose" , "locate" , "ring" , "character" , "insect" , "caught" , "period" , "indicate" , "radio" , "spoke" , "atom" , "human" , "history" , "effect" , "electric" , "expect" , "crop" , "modern" , "element" , "hit" , "student" , "corner" , "party" , "supply" , "bone" , "rail" , "imagine" , "provide" , "agree" , "thus" , "capital" , "won't" , "chair" , "danger" , "fruit" , "rich" , "thick" , "soldier" , "process" , "operate" , "guess" , "necessary" , "sharp" , "wing" , "create" , "neighbor" , "wash" , "bat" , "rather" , "crowd" , "corn" , "compare" , "poem" , "string" , "bell" , "depend" , "meat" , "rub" , "tube" , "famous" , "dollar" , "stream" , "fear" , "sight" , "thin" , "triangle" , "planet" , "hurry" , "chief" , "colony" , "clock" , "mine" , "tie" , "enter" , "major" , "fresh" , "search" , "send" , "yellow" , "gun" , "allow" , "print" , "dead" , "spot" , "desert" , "suit" , "current" , "lift" , "rose" , "continue" , "block" , "chart" , "hat" , "sell" , "success" , "company" , "subtract" , "event" , "particular" , "deal" , "swim" , "term" , "opposite" , "wife" , "shoe" , "shoulder" , "spread" , "arrange" , "camp" , "invent" , "cotton" , "born" , "determine" , "quart" , "nine" , "truck" , "noise" , "level" , "chance" , "gather" , "shop" , "stretch" , "throw" , "shine" , "property" , "column" , "molecule" , "select" , "wrong" , "gray" , "repeat" , "require" , "broad" , "prepare" , "salt" , "nose" , "plural" , "anger" , "claim" , "continent" , "oxygen" , "sugar" , "death" , "pretty" , "skill" , "women" , "season" , "solution" , "magnet" , "silver" , "thank" , "branch" , "match" , "suffix" , "especially" , "fig" , "afraid" , "huge" , "sister" , "steel" , "discuss" , "forward" , "similar" , "guide" , "experience" , "score" , "apple" , "bought" , "led" , "pitch" , "coat" , "mass" , "card" , "band" , "rope" , "slip" , "win" , "dream" , "evening" , "condition" , "feed" , "tool" , "total" , "basic" , "smell" , "valley" , "nor" , "double" , "seat" , "arrive" , "master" , "track" , "parent" , "shore" , "division" , "sheet" , "substance" , "favor" , "connect" , "post" , "spend" , "chord" , "fat" , "glad" , "original" , "share" , "station" , "dad" , "bread" , "charge" , "proper" , "bar" , "offer" , "segment" , "slave" , "duck" , "instant" , "market" , "degree" , "populate" , "chick" , "dear" , "enemy" , "reply" , "drink" , "occur" , "support" , "speech" , "nature" , "range" , "steam" , "motion" , "path" , "liquid" , "log" , "meant" , "quotient" ];
+
+
+
+var ans;
+document.addEventListener('DOMContentLoaded', reset() );
+
+var Wcount = 0 ;
+
+function shuffle( word )
+{
+
+        for ( var i = word.length - 1 ; i > 0 ; i-- )
+            {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = word[i];
+                word[i] = word[j];
+                word[j] = temp;
+            }
+
+}
+
+
+ function reset()
+{
+            shuffle( word );
+
+            var t = "";
+
+            for( i = 0 ; i < 23 ; i++ )
+            {
+               if( i < 22)  t += word[i] +" " ;
+
+               else t += word[i];
+            }
+
+            document.getElementById("para").style.display = "block";
+            document.getElementById("para").innerHTML = '' ;
+
+
+            var nom = 0 ;
+
+            t.split(' ').forEach( x =>
+           {
+                  const cSpan = document.createElement('span');
+                  cSpan.innerText = x;
+
+                  cSpan.classList.add( "th" + nom );   nom++;
+                  document.getElementById("para").appendChild(cSpan);
+                  document.getElementById("para").innerHTML += " ";
+
+           });
+
+            document.getElementById("inputField").value='';
+            document.getElementById("inputField").focus();
+
+
+
+
+             Gcount = 0 ;
+             Gtyped = "";
+             Gi     = 0 ;
+             ans    = 60 ;
+
+             Wcount = 0 ;
+
+             document.getElementById("clock").innerText = "1:00";
+             document.querySelector( ".th" + Gi ).style.backgroundColor = "lightgrey";
+             ison = false ;
+
+
+
+  }
+
+
+ var inp  = document.getElementById("inputField");
+ var Gcount = 0 ;
+ var Gtyped = "";
+ var Gi     = 0 ;
+ ans = 60 ;
+
+var ison = false ;
+
+document.querySelector( ".th" + Gi ).style.backgroundColor = "lightgrey";
+
+document.body.onkeyup = function(e)
+{
+
+
+          if( inp.value != null && e.keyCode != 32 && e.keyCode != 13 )
+          {
+              if( !ison )start();
+
+          }
+
+
+           if( e.keyCode == 32 )
+           {
+
+
+
+              if ( inp.value != null && Gi <= 22  )
+              {
+
+                      Gtyped = inp.value ;
+                      var weshould = document.getElementsByClassName( "th" + Gi ) ;
+
+                      var wesd =     weshould[0].innerHTML + " ";
+
+                      if( Gtyped == wesd )
+                      {
+                          document.querySelector( ".th" + Gi ).style.color = "green";
+
+                          Gcount += wesd.length - 1  ;
+                      }
+
+                      else
+                      {
+                          document.querySelector( ".th" + Gi ).style.color = "tomato";
+                          Wcount += wesd.length - 1  ;
+                      }
+
+
+                      document.querySelector( ".th" + Gi ).style.backgroundColor = "white";
+
+                      Gi++ ;
+                      inp.value = "";
+
+                      if(Gi <= 22 )document.querySelector( ".th" + Gi ).style.backgroundColor = "lightgrey";
+
+                      if( Gi == 23 )reset1();
+
+
+                }
+
+            }
+
+    }
+
+
+
+
+
+
+
+
+function reset1()
+{
+
+
+                    shuffle( word );
+
+                    var t = "";
+
+                    for( i = 0 ; i < 23 ; i++ )
+                    {
+                       if( i < 22)  t += word[i] +" " ;
+
+                       else t += word[i];
+                    }
+
+                    document.getElementById("para").style.display = "block";
+                    document.getElementById("para").innerHTML = '' ;
+
+
+                    var nom = 0 ;
+
+                    t.split(' ').forEach( x =>
+                   {
+                          const cSpan = document.createElement('span');
+                          cSpan.innerText = x;
+
+                          cSpan.classList.add( "th" + nom );   nom++;
+                          document.getElementById("para").appendChild(cSpan);
+                          document.getElementById("para").innerHTML += " ";
+
+                   });
+
+                    document.getElementById("inputField").value='';
+                    document.getElementById("inputField").focus();
+
+                    Gi = 0 ;
+
+                   document.querySelector( ".th" + Gi ).style.backgroundColor = "lightgrey";
+
+
+}
+
+
+var che;
+
+
+
+var p ;
+
+function getTimertime()
+{
+    var n  = new Date();
+    return (  Math.floor(((60000 - ( n - p  ) )/ 1000))     );
+}
+
+
+async function start()
+{
+
+
+     ison = true;
+
+{
+
+     p = new Date();
+
+
+     che = 1 ;
+
+          setInterval( () =>
+
+          {
+
+                 if( che == 1 )
+                 {
+
+
+
+
+                          ans = getTimertime();
+
+                          var gg = document.getElementById("clock") ;
+
+                          if( ans == 0 )
+                          {
+                             che = 2;
+
+
+                             gg.innerText = "0:00";
+
+
+                          }
+
+
+                          else if( che == 1 )
+                          {
+                                var gg = document.getElementById("clock") ;
+
+                                if( ans >= 10 && ans <= 59 )
+                                {
+                                  gg.innerText = "0:" + ans;
+                                }
+                                else
+                                {
+                                  gg.innerText = "0:0" + ans;
+                                }
+
+                           }
+
+
+
+
+
+                           if( che == 2 )
+                           {
+
+                                // console.log("here");
+
+                               document.getElementById("yourspeed").innerText = Math.floor(  (Gcount) / 5  )  + " WPM";
+
+                               document.getElementById("acu").innerText =  "accuracy : "+  Math.floor( ((Gcount) * 100 ) / ( Gcount + Wcount ) )  +"%" ;
+                               document.getElementById("kst").innerText =  "keystokes : " + Gcount + " | " + Wcount ;
+                               che = 3;
+                               document.getElementById("para").style.display = "none";
+
+                               document.getElementById("inputField").blur();
+
+                           }
+
+
+
+                   }
+
+
+              }, 1000 );
+
+
+}
+
+}
